88c66d538b6b51f5e22d05c0db98d7c257fe0847
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-11-30 00:59:20 -0300

    Crenado Slider components

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = "e530ffbc8ead3d338f994287520154e5";
-//https://api.themoviedb.org/3/movie/550?api_key=e530ffbc8ead3d338f994287520154e5
+const api_key = "eb5d41372a40f4c2ed7d8c84aea53e11";
 
-const getTrendingVideos = axios.get(
-  movieBaseUrl + "/trending/all/day?api_key" + api_key
-);
+const getTrendingVideos = () => {
+  return axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+};
 
 export default {
   getTrendingVideos,
 };
+//https://api.themoviedb.org/3/trending/all/day?api_key=eb5d41372a40f4c2ed7d8c84aea53e11
